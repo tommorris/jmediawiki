6277ddf2d8e5c8357513b26f376b30bb9818ef4e
Tom Morris <tom@tommorris.org>
2012-07-17 04:18:29 +0100

    adding Javadoc on Namespace.java

--- src/org/tommorris/jmediawiki/Namespace.java
+++ src/org/tommorris/jmediawiki/Namespace.java
@@ -2,6 +2,16 @@ package org.tommorris.jmediawiki;
 import nu.xom.Element;
 import lombok.*;
 
+/**
+ * Data class for representing MediaWiki namespaces.
+ * 
+ * A MediaWiki namespace is a grouping of pages used to split up the wiki into
+ * separate sections. For instance, on Wikipedia, namespaces are used to split
+ * articles off from talk pages, project pages and so on.
+ * 
+ * @author tom
+ * 
+ */
 public class Namespace {
 	@Getter @Setter private Integer id = null;
 	@Getter @Setter private String canonical;
